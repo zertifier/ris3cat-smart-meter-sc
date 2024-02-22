3ff978769a1aa9e69e26b7584b5f6ea493052361
Juan Alejandro Marin Ruiz <amarin@zertifier.com>
2024-02-22 11:25:04 +0100

    feat:login page finished

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {OauthLoginComponent} from "../../components/oauth-login/oauth-login.component";
 import {NgbModal, NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOutlet} from "@ng-bootstrap/ng-bootstrap";
 import {WebWalletLoginComponent} from "../../components/web-wallet-login/web-wallet-login.component";
+import {LoginImagesService} from "../../services/login-images.service";
+import {NgOptimizedImage} from "@angular/common";
 
 export enum LoginTabs {
   OAUTH_TAB = 1,
@@ -18,17 +20,19 @@ export enum LoginTabs {
     NgbNavLinkButton,
     WebWalletLoginComponent,
     NgbNavContent,
-    NgbNavOutlet
+    NgbNavOutlet,
+    NgOptimizedImage
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
   public activeId: number = LoginTabs.OAUTH_TAB;
+  public imageUrl = this.loginImagesService.getRandomImage();
 
   public googleActive: boolean = false;
 
-  constructor(private ngbModal: NgbModal) {
+  constructor(private ngbModal: NgbModal, public loginImagesService: LoginImagesService) {
   }
 
   protected readonly LoginTabs = LoginTabs;
---
+++ src/app/features/auth/services/login-images.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginImagesService {
+  bgImages = [
+    'assets/img/login/bg1.jpg',
+    'assets/img/login/bg2.jpg',
+    'assets/img/login/bg3.jpg',
+    'assets/img/login/bg4.jpg',
+    'assets/img/login/bg5.jpg',
+    'assets/img/login/bg6.jpg',
+  ]
+  constructor() { }
+
+  public getRandomImage(): string {
+    const index = this.randomNumber(0, this.bgImages.length - 1);
+    return this.bgImages[index];
+  }
+
+  /**
+   * Return random number between min and max both included in range
+   * @param min
+   * @param max
+   */
+  randomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
