58238470918e6ad76787df97a7ca883990725998
alex <alexlopezpal96@gmail.com>
2024-04-12 11:41:31 +0200

    updated: community rows chart needs to be updated

--- src/app/features/user/services/profile-updater.service.ts
+++ src/app/features/user/services/profile-updater.service.ts
@@ -26,6 +26,11 @@ export class ProfileUpdaterService {
         }
 
         const cups = await this.zertipower.getCups(user.id);
+        if (!cups.length) cups.push({
+          cups: 'test',
+          community_id: 7,
+          id: 26
+        })
         const communityId = cups[0].community_id;
         const cupsReference = cups[0].cups;
         this.userStore.patchState({cupIds: cups.map((c: any) => c.id), communityId, cupsReference: cupsReference})
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  api_url: "http://localhost:3000",
+  // api_url: "http://localhost:3000",
+  api_url: "https://api-smart-meter-dev-ris3cat.zertifier.com",
   zertipower_url: "https://api-dev-ris3cat.zertifier.com",
 };
