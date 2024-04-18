724bbf87b636c10739fc035b5be01c5346692d1d
4strodev <jumarurex@gmail.com>
2024-04-18 09:47:17 +0200

    feat:missing community page done

--- src/app/features/energy-stats/infrastructure/pages/missing-community-page/missing-community-page.component.ts
+++ src/app/features/energy-stats/infrastructure/pages/missing-community-page/missing-community-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {
+  QuestionBadgeComponent
+} from "../../../../../shared/infrastructure/components/question-badge/question-badge.component";
+import {NavbarComponent} from "../../../../../shared/infrastructure/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-missing-community-page',
   standalone: true,
-  imports: [],
+  imports: [
+    QuestionBadgeComponent,
+    NavbarComponent
+  ],
   templateUrl: './missing-community-page.component.html',
   styleUrl: './missing-community-page.component.scss'
 })
