849ec02d1b0fa4cb9841becc17d128c507022f70
4strodev <jumarurex@gmail.com>
2024-06-13 11:40:36 +0200

    feat:use interceptors to sync localstorage with auth store

--- src/app/features/auth/infrastructure/services/auth-store.service.ts
+++ src/app/features/auth/infrastructure/services/auth-store.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {RxStore} from "@zertifier/rx-store";
 import {jwtDecode, JwtPayload} from 'jwt-decode';
-import {EventBus} from "@shared/domain/EventBus";
 
 export interface AuthState {
   accessToken: string;
@@ -36,22 +35,17 @@ export class AuthStoreService extends RxStore<AuthState> {
   }
 
   constructor(
-    private readonly eventBus: EventBus,
   ) {
     super(defaultValues);
   }
 
   // TODO move local storage interaction to persistence proxy
   public removeTokens() {
-    localStorage.removeItem(ACCESS_TOKEN);
-    localStorage.removeItem(REFRESH_TOKEN);
     this.patchState({accessToken: '', refreshToken: '', authData: undefined})
   }
 
   public setTokens({refreshToken, accessToken}: { refreshToken: string, accessToken: string }) {
     const decodedToken = this.decodeToken(refreshToken);
-    localStorage.setItem(ACCESS_TOKEN, accessToken);
-    localStorage.setItem(REFRESH_TOKEN, refreshToken);
     this.patchState({refreshToken, accessToken, authData: decodedToken});
   }
 
