14dbe4015a90cd038f62bfe37ccf9833249a2963
alex <alexlopezpal96@gmail.com>
2024-06-20 14:26:46 +0200

    updated: swal styles
    added: join community modal

--- src/app/features/energy-stats/infrastructure/pages/community/missing-community-page/missing-community-page.component.ts
+++ src/app/features/energy-stats/infrastructure/pages/community/missing-community-page/missing-community-page.component.ts
@@ -3,17 +3,29 @@ import {
   QuestionBadgeComponent
 } from "../../../../../../shared/infrastructure/components/question-badge/question-badge.component";
 import {NavbarComponent} from "../../../../../../shared/infrastructure/components/navbar/navbar.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {JoinCommunityModalComponent} from "./join-community-modal/join-community-modal.component";
 
 @Component({
   selector: 'app-missing-community-page',
   standalone: true,
   imports: [
     QuestionBadgeComponent,
-    NavbarComponent
+    NavbarComponent,
+
   ],
   templateUrl: './missing-community-page.component.html',
   styleUrl: './missing-community-page.component.scss'
 })
 export class MissingCommunityPageComponent {
+  constructor(
+    private readonly modalService: NgbModal
+  ) {
+  }
+
+
+  openJoinModal(){
+    const modalRef = this.modalService.open(JoinCommunityModalComponent)
+  }
 
 }
