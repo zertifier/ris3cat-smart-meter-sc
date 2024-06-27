d6d423724df1e0e6409ef54c67535054d03bb0be
4strodev <jumarurex@gmail.com>
2024-06-27 14:24:03 +0200

    fix:change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api_url: "https://api-smart-meter-ris3cat.zertifier.com",
-  zertipower_url: "https://api-ris3cat.zertifier.com",
+  zertipower_url: "https://api.zertipower.com",
   zertirpcs_url: "https://zertirpc.zertifier.com",
   defaultRpc: 'https://rpc.ankr.com/gnosis',
   erc20_contract: "0x354F8703d5755BF063FC9018D073c1cFc069Ec46",
