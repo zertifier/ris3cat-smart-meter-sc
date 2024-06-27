7c6e329efbad32785a787188d8b98aa33df30880
alex <alexlopezpal96@gmail.com>
2024-06-27 11:19:19 +0200

    updated: weather to catalan

--- src/app/features/energy-stats/infrastructure/components/metereologic-prediction/metereologic-prediction.component.ts
+++ src/app/features/energy-stats/infrastructure/components/metereologic-prediction/metereologic-prediction.component.ts
@@ -1,21 +1,30 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, LOCALE_ID, OnInit} from '@angular/core';
 import {EnergyPredictionChartComponent} from "./metereologic-chart/energy-prediction-chart.component";
 import {ChartDataset} from "@shared/infrastructure/interfaces/ChartDataset";
 import {StatsColors} from "../../../domain/StatsColors";
 import {WeatherPredictionService} from "../../services/weather-prediction.service";
 import {EnergyPredictionService} from "../../services/energy-prediction.service";
 import dayjs from 'dayjs';
+import {DatePipe, registerLocaleData} from "@angular/common";
+import localeCa from '@angular/common/locales/ca';
 
 @Component({
   selector: 'app-metereologic-prediction',
   standalone: true,
   imports: [
-    EnergyPredictionChartComponent
+    EnergyPredictionChartComponent,
+    DatePipe
   ],
+  providers: [{provide: LOCALE_ID, useValue: 'ca-ES'}],
   templateUrl: './metereologic-prediction.component.html',
   styleUrl: './metereologic-prediction.component.scss'
 })
+
 export class MetereologicPredictionComponent implements OnInit {
+  constructor() {
+    // Register the Catalan locale data
+    registerLocaleData(localeCa);
+  }
   weatherPredictionService = inject(WeatherPredictionService)
   elements: {
     label: string
