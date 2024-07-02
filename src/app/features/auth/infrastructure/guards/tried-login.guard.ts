4676956a4f2a54c28df837616a661308afaf77e2
4strodev <jumarurex@gmail.com>
2024-07-02 09:32:13 +0200

    docs:document triedLoginGuard

--- src/app/features/auth/infrastructure/guards/tried-login.guard.ts
+++ src/app/features/auth/infrastructure/guards/tried-login.guard.ts
@@ -2,6 +2,9 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthStoreService} from "../services/auth-store.service";
 
+/**
+ * The user is allowed to access to register page only if tries to log in and the user does not exist
+ */
 export const triedLoginGuard: CanActivateFn = () => {
   const authStore = inject(AuthStoreService);
   const router = inject(Router);
