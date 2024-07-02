9dbd7956baf29257d4dbdc78a45ee385c5e52acb
4strodev <jumarurex@gmail.com>
2024-07-02 12:40:31 +0200

    docs:document actions and domain of energy-stats

--- src/app/features/energy-stats/domain/ChartEntity.ts
+++ src/app/features/energy-stats/domain/ChartEntity.ts
@@ -1,3 +1,7 @@
+/**
+ * Users want to see two type of charts. Community information charts and individual cups chart.
+ * This enum specifies the kind of information groups that chart should show.
+ */
 export enum ChartEntity {
   CUPS = 'cups',
   COMMUNITIES = 'communities'
